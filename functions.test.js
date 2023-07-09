e3a71417e59a5f0e8677fbede926e3cf9511ad46
alecnissen <alecnissen@yahoo.com>
2023-07-09 19:27:26 -0400

    more practice with jest and using it's methods

--- functions.test.js
+++ functions.test.js
@@ -1,4 +1,29 @@
 const functions = require('./functions');
+
+// beforeEach(() => initDatabase());
+// afterEach(() => closeDatabase());
+
+// beforeAll(() => initDatabase());
+// afterAll(() => closeDatabase());
+
+// const initDatabase = () => console.log('Database Initialized...');
+// const closeDatabase = () => console.log('Database Closed...'); 
+const nameCheck = () => console.log('Checking Name....');
+
+describe('Checking Names', () => {
+    beforeEach(() => nameCheck());
+
+    test('User is Jeff', () => { 
+        const user = 'Jeff';
+        expect(user).toBe('Jeff');
+    });
+
+    test('User is Karen', () => { 
+        const user = 'Karen';
+        expect(user).toBe('Karen');
+    });
+});
+
 // toBe
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
@@ -43,5 +68,23 @@ test('There is no I in team', () => {
 test('Admin should be in username', () => { 
     usernames = ['john', 'karen', 'admin'];
     expect(usernames).toContain('admin');
-})
+}); 
 
+// working with async data 
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//     expect.assertions(1);
+//     return functions.fetchUser()
+//     .then(data => { 
+//         expect(data.name).toEqual('Leanne Graham');
+//     });
+// });
+
+// async await 
+
+test('User fetched name should be Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+}); 
